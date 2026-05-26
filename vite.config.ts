87fa4b063be793b2afcb55d07189c579dac3998d
Carlos Ramos <carlosramosdev@protonmail.com>
2026-05-26 16:45:35 +0200

    implemented tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,20 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react, { reactCompilerPreset } from '@vitejs/plugin-react';
+import babel from '@rolldown/plugin-babel';
+import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()] }),
+    tailwindcss(),
   ],
-    resolve: {
+  resolve: {
     alias: {
-      '@': path.resolve(__dirname, "./src"),
-      '@mocks': path.resolve(__dirname, "./test/mocks"),
-    }
-  }
-})
+      '@': path.resolve(__dirname, './src'),
+      '@mocks': path.resolve(__dirname, './test/mocks'),
+    },
+  },
+});
