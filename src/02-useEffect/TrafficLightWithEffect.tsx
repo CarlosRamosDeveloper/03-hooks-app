bafcd6c3bbf1700d3c6e7f4510c912ca8e4a1477
Carlos Ramos <carlosramosdev@protonmail.com>
2026-05-26 17:58:25 +0200

    implemented color autoswap implemented countdown

--- src/02-useEffect/TrafficLightWithEffect.tsx
+++ src/02-useEffect/TrafficLightWithEffect.tsx
@@ -12,9 +12,8 @@ export const TrafficLightWithEffect = () => {
   const [light, setLight] = useState<TrafficLightColor>('red');
   const [countdown, setCountdown] = useState(5);
 
+  // Countdown Effect
   useEffect(() => {
-    if (countdown === 0) return;
-
     const intervalId = setInterval(() => {
       setCountdown((prev) => prev - 1);
     }, 1000);
@@ -22,13 +21,38 @@ export const TrafficLightWithEffect = () => {
     return () => {
       clearInterval(intervalId);
     };
-  }, [countdown]);
+  }, []);
+
+  useEffect(() => {
+    if (countdown > 0) return;
+
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    setCountdown(5);
+    switch (light) {
+      case 'red':
+        setLight('green');
+        return;
+
+      case 'yellow':
+        setLight('red');
+        return;
+      case 'green':
+        setLight('yellow');
+        return;
+    }
+  }, [countdown, light]);
 
   return (
     <div className="min-h-screen bg-linear-to-br from-slate-900 via-gray-900 to-slate-800 flex items-center justify-center p-4">
       <div className="flex flex-col items-center space-y-8">
         <h1 className="text-white text-2xl">Semaforo con useEffect</h1>
         <h2 className="text-white text-xl">{countdown}</h2>
+        <div className="w-64 bg-gray-700 rounded-full h-2">
+          <div
+            className="bg-blue-500 h-2 rounded-full transition-all duration-1000"
+            style={{ width: `${(countdown / 5) * 100}%` }}
+          ></div>
+        </div>
         <div
           className={`w-32 h-32 ${light === 'red' ? colors[light] : 'bg-gray-500'} rounded-full`}
         ></div>
