5ab83c5f34bc7381175a819a5da84f5de680edfc
Carlos Ramos <carlosramosdev@protonmail.com>
2026-05-26 16:36:15 +0200

    Implemented App component

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-createRoot(document.getElementById("root")!).render(<StrictMode></StrictMode>);
+import { HooksApp } from './HooksApp';
+
+import './index.css';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <HooksApp />
+  </StrictMode>,
+);
