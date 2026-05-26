ea11058115c598ddd3bed347c8c269e4fedda3e0
Carlos Ramos <carlosramosdev@protonmail.com>
2026-05-26 17:31:43 +0200

    implemented countdown effect at trafficLights v2

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-// import { HooksApp } from './HooksApp';
+import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect';
 
 import './index.css';
-import { TrafficLight } from './01-useState/TraficLight';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     {/* <HooksApp /> */}
-    <TrafficLight />
+    <TrafficLightWithEffect />
   </StrictMode>,
 );
