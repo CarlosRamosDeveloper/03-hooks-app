c8dbff39a252830805d334d0e311580a6505f6a6
Carlos Ramos <carlosramosdev@protonmail.com>
2026-05-29 20:42:30 +0200

    implemented client information

---
+++ src/08-use-suspense/LoadingComponent.tsx
@@ -0,0 +1,7 @@
+export const LoadingComponent = () => {
+  return (
+    <div className="bg-gradient flex flex-col">
+      <h1 className="text-2xl">Cargando...</h1>
+    </div>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
-import { StrictMode } from 'react';
+import { StrictMode, Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import './index.css';
 
 import { Toaster } from 'sonner';
 import { ClientInformation } from './08-use-suspense/ClientInformation';
+import { LoadingComponent } from './08-use-suspense/LoadingComponent';
+import { getUserAction } from './08-use-suspense/api/get-user-action';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Toaster />
-    <ClientInformation />
+    <Suspense fallback={<LoadingComponent />}>
+      <ClientInformation getUser={getUserAction(1000)} />
+    </Suspense>
   </StrictMode>,
 );
