fca705260d61c4b7d44b164fb7e70b17d222f7eb
Carlos Ramos <carlosramosdev@protonmail.com>
2026-05-31 16:30:19 +0200

    improved QoL

--- src/09-useContext/pages/about/AboutPage.tsx
+++ src/09-useContext/pages/about/AboutPage.tsx
@@ -1,18 +1,30 @@
+import { UserContext } from '@/09-useContext/context/UserContext';
+import { Button } from '@/components/ui/button';
+import { use } from 'react';
 import { Link } from 'react-router';
 
 export const AboutPage = () => {
+  const { authStatus, logout } = use(UserContext);
+  const isAuthenticated = authStatus === 'authenticated';
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-4xl font-bold">Página sobre mi</h1>
       <hr />
-
-      <div className="flex flex-col gap-2">
+      {isAuthenticated && (
         <Link className="hover:text-blue-500 underline text-2xl" to="/profile">
           Perfil
         </Link>
-        <Link className="hover:text-blue-500 underline text-2xl" to="/login">
-          Iniciar sesión
-        </Link>
+      )}
+      <div className="flex flex-col gap-2">
+        {isAuthenticated ? (
+          <Button variant="destructive" onClick={logout}>
+            Salir
+          </Button>
+        ) : (
+          <Link className="hover:text-blue-500 underline text-2xl" to="/login">
+            Iniciar sesión
+          </Link>
+        )}
       </div>
     </div>
   );
