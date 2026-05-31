45a72056b69238e0b4191abf325f590facab9e1e
Carlos Ramos <carlosramosdev@protonmail.com>
2026-05-31 15:39:37 +0200

    added modk data Updated UserContextProvider

--- src/09-useContext/context/UserContext.tsx
+++ src/09-useContext/context/UserContext.tsx
@@ -1,11 +1,46 @@
-import React, { useState } from 'react';
+import React, { createContext, useState } from 'react';
+import type { User } from '../data/user.mock.data';
 
-interface UserContextProps {
+type AuthStatus = 'checking' | 'authenticated' | 'non-authenticated';
+
+interface Props {
   children: React.ReactNode;
 }
 
-export const UserContextProvider = ({ children }: UserContextProps) => {
-  const [user, setUser] = useState('Fernando');
+interface UserContextProps {
+  authStatus: AuthStatus;
+  user: User | null;
+  login: (userId: number) => boolean;
+  logout: () => void;
+}
 
-  return <>{children}</>;
+export const UserContext = createContext<UserContextProps>(
+  {} as UserContextProps,
+);
+
+export const UserContextProvider = ({ children }: Props) => {
+  const [authStatus, setAuthStatus] = useState<AuthStatus>('checking');
+  const [user, setUser] = useState<User | null>(null);
+
+  const handleLogin = (userId: number): boolean => {
+    console.log(`User id: ${userId}`);
+    return true;
+  };
+
+  const handleLogout = () => {
+    console.log('Logging out!');
+  };
+
+  return (
+    <UserContext
+      value={{
+        authStatus: authStatus,
+        user: user,
+        login: handleLogin,
+        logout: handleLogout,
+      }}
+    >
+      {children}
+    </UserContext>
+  );
 };
