c2ea45e4282d5a0a8c8f44459182935d7555e8cd
Carlos Ramos <carlosramosdev@protonmail.com>
2026-05-31 14:20:32 +0200

    Added placeholder

---
+++ src/09-useContext/ProfessionalApp.tsx
@@ -0,0 +1,9 @@
+export const ProfessionalApp = () => {
+  return (
+    <>
+      <div className="bg-gradient flex flex-col gap-4">
+        <h1>Professional App</h1>
+      </div>
+    </>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,17 @@
-import { StrictMode, Suspense } from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import './index.css';
 
 import { Toaster } from 'sonner';
-import { ClientInformation } from './08-use-suspense/ClientInformation';
-import { LoadingComponent } from './08-use-suspense/LoadingComponent';
-import { getUserAction } from './08-use-suspense/api/get-user-action';
+import { ProfessionalApp } from './09-useContext/ProfessionalApp';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Toaster />
-    <Suspense fallback={<LoadingComponent />}>
+    {/* <Suspense fallback={<LoadingComponent />}>
       <ClientInformation getUser={getUserAction(1000)} />
-    </Suspense>
+    </Suspense> */}
+    <ProfessionalApp />
   </StrictMode>,
 );
